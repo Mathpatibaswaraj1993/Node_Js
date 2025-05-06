fe572815b972d959d7adc6708ccb77f3ddda2afc
https://github.com/Mathpatibaswaraj1993/HTMLProject.git <mathpatibaswaraj@gmail.com>
2025-05-06 09:38:05 +0530

    project

---
+++ NodeExpress/public/EjsDemo.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+const user = {
+    name: "john",
+    age: 23,
+    country: "US",
+    role:"Manager"
+}
+
+app.listen(3000,() => {
+    
+})
